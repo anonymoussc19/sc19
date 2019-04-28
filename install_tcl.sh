34552d90b491f561529aef9a26e13f22f31520e9
mikumiku <hatsune@miku>
2019-04-28 17:09:56 -0400

    README

--- install_tcl.sh
+++ install_tcl.sh
@@ -12,6 +12,7 @@ sed -i 's/#BLAS_LIB/BLAS_LIB/g' Makefile
 sed -i 's/#INCLUDE_FLAGS/INCLUDE_FLAGS/g' Makefile
 sed -i 's/BLAS_LIB=-L${BLIS/#BLAS_LIB=-L${BLIS/g' Makefile
 
+# Change according to you MKL
 source /opt/software/intel/parallel_studio_xe_2018_cluster_edition/compilers_and_libraries_2018/linux/bin/compilervars.sh intel64 
 make 
 echo $TCL_ROOT=`pwd`
